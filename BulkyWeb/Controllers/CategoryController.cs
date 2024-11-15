c5e36079a177d585835984e91dc9e2aa3676ec12
littlecub125 <yubi2023@gmail.com>
2024-11-15 19:15:27 +0900

    update
    added "Category" Model
    added Category view

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db=db;
+        }
+
+        public IActionResult Index()
+        {
+            var obj = _db.Categories.ToList();
+            return View(obj);
+        }
+    }
+}
